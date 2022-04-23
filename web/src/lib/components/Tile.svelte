ca780edd058492250543cd287af4548b9d25e7bd
fraserdscott <fraserdscott@gmail.com>
2022-04-23 15:49:08 +0200

    Refactor village popup

--- web/src/lib/components/Tile.svelte
+++ web/src/lib/components/Tile.svelte
@@ -46,45 +46,70 @@
     : 'border-black-500'}"
 >
   {#if showModal}
-    <Modal title={`Tile ${x},${y}`} on:close={() => (showModal = false)} closeButton={true}>
-      {#if village}
-        <div>This tile is a village.</div>
-        <div class="border-2">
-          <div>Name: {village.name}</div>
-          <div>Description: {village.description}</div>
+    <Modal title={`${village ? 'Village' : 'Tile'} ${x},${y}`} on:close={() => (showModal = false)} closeButton={true}>
+      <div class="flex flex-col">
+        <div class="m-2 rounded-md border-4 p-4">
+          <div class="text-xl">Overview</div>
+          {#if village}
+            <div>
+              <div>Name: {village.name}</div>
+              <div>Description: {village.description}</div>
+            </div>
+          {:else}
+            <div>
+              <div>Name: None.</div>
+              <div>Description: This tile is an empty field.</div>
+            </div>
+          {/if}
         </div>
-      {:else}
-        <div>This tile is an empty field.</div>
-      {/if}
-      <div>There are {players.length} player(s) in this square.</div>
-      {#if currentPlayer && !currentPlayer.arrivalTime}
-        <div>
-          This tile is {distance(x, y, currentPlayer.x, currentPlayer.y)} units away. It will take
-          <span class="font-bold"
-            >{DISTANCE_MULTIPLIER} * {distance(x, y, currentPlayer.x, currentPlayer.y)} = {DISTANCE_MULTIPLIER *
-              distance(x, y, currentPlayer.x, currentPlayer.y)}</span
-          > seconds to travel to.
+        <div class="m-2 rounded-md border-4 p-4 ">
+          <div class="text-xl">Players</div>
+          <div class="italic">There are {players.length} player(s) in this square.</div>
+          <ul class="list-disc">
+            {#each players as player}
+              <li>
+                <a href={`https://etherscan.io/address/${player.id}`}>{player.id.slice(0, 6)}...{player.id.slice(-4)}</a
+                >
+              </li>
+            {/each}
+          </ul>
         </div>
-      {/if}
-      <button
-        on:click={() => beginMove(x, y)}
-        disabled={currentPlayer.arrivalTime}
-        class="flex-shrink-0 bg-yellow-500 hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600 text-sm border-4
+
+        <div class="m-2 rounded-md border-4 p-4">
+          <div class="text-xl">Actions</div>
+          <div class="rounded-md border-2 p-4">
+            <div class="text-lg">Movement</div>
+            {#if currentPlayer && !currentPlayer.arrivalTime}
+              <div>
+                This tile is {distance(x, y, currentPlayer.x, currentPlayer.y)} units away. It will take
+                <span class="font-bold"
+                  >{DISTANCE_MULTIPLIER} * {distance(x, y, currentPlayer.x, currentPlayer.y)} = {DISTANCE_MULTIPLIER *
+                    distance(x, y, currentPlayer.x, currentPlayer.y)}</span
+                > seconds to travel to.
+              </div>
+            {/if}
+            <button
+              on:click={() => beginMove(x, y)}
+              disabled={currentPlayer.arrivalTime}
+              class="flex-shrink-0 bg-yellow-500 hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600 text-sm border-4
         text-white py-1 px-2 rounded disabled:bg-gray-400 disabled:border-gray-400 disabled:cursor-not-allowed"
-        type="button"
-      >
-        BEGIN MOVE
-      </button>
-      {#if currentPlayer.arrivalTime && currentPlayer.xDestination === x && currentPlayer.yDestination === y}
-        <button
-          class="flex-shrink-0 bg-yellow-500 hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600 text-sm border-4
+              type="button"
+            >
+              BEGIN MOVE
+            </button>
+            {#if currentPlayer.arrivalTime && currentPlayer.xDestination === x && currentPlayer.yDestination === y}
+              <button
+                class="flex-shrink-0 bg-yellow-500 hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600 text-sm border-4
       text-white py-1 px-2 rounded disabled:bg-gray-400 disabled:border-gray-400 disabled:cursor-not-allowed"
-          type="button"
-          disabled={currentPlayer.arrivalTime > currentTimestamp / 1000}
-          on:click={resolveMove}
-          >RESOLVE MOVE ({Math.round(currentPlayer.arrivalTime - currentTimestamp / 1000)}s)</button
-        >
-      {/if}
+                type="button"
+                disabled={currentPlayer.arrivalTime > currentTimestamp / 1000}
+                on:click={resolveMove}
+                >RESOLVE MOVE ({Math.round(currentPlayer.arrivalTime - currentTimestamp / 1000)}s)</button
+              >
+            {/if}
+          </div>
+        </div>
+      </div>
     </Modal>
   {/if}
   <svg width="100" on:click={() => (showModal = true)}>
