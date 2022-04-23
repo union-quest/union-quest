ae8b29870ff6aca05c3d80ba8a19a3d800b91164
fraserdscott <fraserdscott@gmail.com>
2022-04-23 12:21:54 +0200

    Update css

--- web/src/routes/map.svelte
+++ web/src/routes/map.svelte
@@ -41,45 +41,49 @@
       x: {$player.data.x}
       y: {$player.data.y}
 
-      <table>
-        {#each [0, 1, 2, 3, 4, 5] as i}
-          <tr>
-            {#each [0, 1, 2, 3, 4, 5] as j}
-              <td on:click={() => move(i, j)}>
-                <svg width="100">
+      <div class="grid grid-cols-6 w-fit">
+        {#each [0, 1, 2, 3, 4, 5] as x}
+          {#each [0, 1, 2, 3, 4, 5] as y}
+            <div
+              class="border-4 hover:border-green-500 {$players.data.find((v) => v.x === x && v.y === y) &&
+              $players.data.find((v) => v.x === x && v.y === y).id === $wallet.address.toLowerCase()
+                ? 'border-yellow-500 hover:border-yellow-500'
+                : 'border-black-500'}"
+              on:click={() => move(x, y)}
+            >
+              <svg width="100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
+                  <rect fill="#009A17" width="1" height="1" />
+                  <path d="M 0.161,0.178 0.249,0.336" fill="none" stroke="#004400" stroke-width="0.02" />
+                  <path d="M 0.314,0.173 V 0.33" fill="none" stroke="#004400" stroke-width="0.02" />
+                  <path d="M 0.469,0.178 0.376,0.336" fill="none" stroke="#004400" stroke-width="0.02" />
+                  <path d="m 0.48277796,0.53428125 0.088,0.158" fill="none" stroke="#004400" stroke-width="0.02" />
+                  <path d="m 0.63577796,0.52928125 v 0.157" fill="none" stroke="#004400" stroke-width="0.02" />
+                  <path d="m 0.79077796,0.53428125 -0.093,0.158" fill="none" stroke="#004400" stroke-width="0.02" />
+                </svg>
+                {#if $villages.data.some((v) => v.x === x && v.y === y)}
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
-                    <rect fill="#009A17" width="1" height="1" />
-                    <path d="M 0.161,0.178 0.249,0.336" fill="none" stroke="#004400" stroke-width="0.02" />
-                    <path d="M 0.314,0.173 V 0.33" fill="none" stroke="#004400" stroke-width="0.02" />
-                    <path d="M 0.469,0.178 0.376,0.336" fill="none" stroke="#004400" stroke-width="0.02" />
-                    <path d="m 0.48277796,0.53428125 0.088,0.158" fill="none" stroke="#004400" stroke-width="0.02" />
-                    <path d="m 0.63577796,0.52928125 v 0.157" fill="none" stroke="#004400" stroke-width="0.02" />
-                    <path d="m 0.79077796,0.53428125 -0.093,0.158" fill="none" stroke="#004400" stroke-width="0.02" />
+                    <polygon fill="gold" points="0.25,0.35 0.5,0.1 0.75,0.35" />
+                    <rect fill="darkkhaki" x="0.25" y="0.35" width="0.5" height="0.5" />
+                    <rect fill="brown" x="0.45" y="0.65" width="0.1" height="0.2" />
                   </svg>
-                  {#if $villages.data.some((v) => v.x === i && v.y === j)}
+                {/if}
+                {#if $players.data.some((v) => v.x === x && v.y === y)}
+                  {#if $players.data.find((v) => v.x === x && v.y === y).id === $wallet.address.toLowerCase()}
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
-                      <polygon fill="gold" points="0.25,0.35 0.5,0.1 0.75,0.35" />
-                      <rect fill="darkkhaki" x="0.25" y="0.35" width="0.5" height="0.5" />
-                      <rect fill="brown" x="0.45" y="0.65" width="0.1" height="0.2" />
+                      <circle fill="white" r="0.25" cx="0.5" cy="0.5" />
+                    </svg>
+                  {:else}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
+                      <circle fill="black" r="0.25" cx="0.5" cy="0.5" />
                     </svg>
                   {/if}
-                  {#if $players.data.some((v) => v.x === i && v.y === j)}
-                    {#if $players.data.find((v) => v.x === i && v.y === j).id === $wallet.address.toLowerCase()}
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
-                        <circle fill="white" r="0.25" cx="0.5" cy="0.5" />
-                      </svg>
-                    {:else}
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
-                        <circle fill="black" r="0.25" cx="0.5" cy="0.5" />
-                      </svg>
-                    {/if}
-                  {/if}
-                </svg>
-              </td>
-            {/each}
-          </tr>
+                {/if}
+              </svg>
+            </div>
+          {/each}
         {/each}
-      </table>
+      </div>
     {/if}
   </section>
 </WalletAccess>
