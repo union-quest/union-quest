05ea26029ed45c4984f7cbec1e0986a55e91e0da
fraserdscott <fraserdscott@gmail.com>
2022-05-15 23:31:39 +0100

    Add basic touch screen controls

--- web/src/lib/components/MapCanvas.svelte
+++ web/src/lib/components/MapCanvas.svelte
@@ -136,6 +136,34 @@
     }
   }
 
+  function handleTouch(e, singleTouchHandler) {
+    if (e.touches.length == 1) {
+      singleTouchHandler(e);
+    } else if (e.type == 'touchmove' && e.touches.length == 2) {
+      isDragging = false;
+      handlePinch(e);
+    }
+  }
+
+  let initialPinchDistance = null;
+  let lastZoom = cameraZoom;
+
+  function handlePinch(e) {
+    e.preventDefault();
+
+    let touch1 = {x: e.touches[0].clientX, y: e.touches[0].clientY};
+    let touch2 = {x: e.touches[1].clientX, y: e.touches[1].clientY};
+
+    // This is distance squared, but no need for an expensive sqrt as it's only used in ratio
+    let currentDistance = (touch1.x - touch2.x) ** 2 + (touch1.y - touch2.y) ** 2;
+
+    if (initialPinchDistance == null) {
+      initialPinchDistance = currentDistance;
+    } else {
+      adjustZoom(null, currentDistance / initialPinchDistance);
+    }
+  }
+
   function adjustZoom(evt) {
     cameraZoom -= evt.deltaY;
     cameraZoom = Math.min(cameraZoom, MAX_ZOOM);
@@ -153,16 +181,13 @@
       },
       false
     );
-    canvas.addEventListener(
-      'wheel',
-      function (evt) {
-        adjustZoom(evt);
-      },
-      false
-    );
-    canvas.addEventListener('mousemove', onPointerMove);
     canvas.addEventListener('mousedown', onPointerDown);
+    canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown));
     canvas.addEventListener('mouseup', onPointerUp);
+    canvas.addEventListener('touchend', (e) => handleTouch(e, onPointerUp));
+    canvas.addEventListener('mousemove', onPointerMove);
+    canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove));
+    canvas.addEventListener('wheel', (e) => adjustZoom(e));
 
     const interval = setInterval(() => {
       currentTimestamp = Date.now();
