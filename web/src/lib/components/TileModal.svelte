40f159b9af7582ec4c1955f156b8e9938352fede
fraserdscott <fraserdscott@gmail.com>
2022-05-17 01:36:57 +0100

    Set manual gasLimit on move() txs

--- web/src/lib/components/TileModal.svelte
+++ web/src/lib/components/TileModal.svelte
@@ -14,7 +14,7 @@
   const roundGood = (n: number) => Math.round(n * 10) / 10;
 
   async function move(x, y) {
-    await flow.execute((contracts) => contracts.UnionQuest.move(x, y));
+    await flow.execute((contracts) => contracts.UnionQuest.move(x, y, {gasLimit: '100000'}));
   }
 
   let currentTimestamp = Date.now();
